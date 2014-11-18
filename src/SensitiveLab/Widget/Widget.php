27c61fc9fe922b5358b4a5fbeff12698d916d6d4
Patrick Dorio <patrick.dorio@sensitive-lab.fr>
2014-11-18 12:43:15 +0000

    Initial Commit

---
+++ src/SensitiveLab/Widget/Widget.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Patrick
+ * Date: 18/11/2014
+ * Time: 13:10
+ */
+
+namespace SensitiveLab\Facades;
+
+
+
+class Widget
+{
+
+    public function coucou() {
+        dd('coucou');
+    }
+
+}
